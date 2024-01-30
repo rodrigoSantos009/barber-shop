a8abca0667890f8ea142c619298ac1a6435ef312
rodrigoSantos009 <009rodrigo009@gmail.com>
2024-01-30 13:29:05 -0300

    add inital babershop item

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,11 +3,17 @@ import Header from "../_components/header";
 import { format } from "date-fns";
 import { Search } from "./__components/search";
 import { BookingItem } from "../_components/booking-item";
+import { db } from "../_lib/prisma";
+import { BarberShopItem } from "./__components/barbershop-item";
 
-export default function Home() {
+export default async function Home() {
+  // call prisma and take barbershops
+  const barbershops = await db.barbershop.findMany({});
+
   return (
     <div>
       <Header />
+
       <div className="px-5 pt-5">
         <h2 className="text-xl font-bold">Olá, Rodrigo!</h2>
         <p className="capitalize text-sm">
@@ -20,8 +26,21 @@ export default function Home() {
         <Search />
       </div>
       <div className="px-5 mt-6">
-          <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
-          <BookingItem />
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">
+          Agendamentos
+        </h2>
+        <BookingItem />
+      </div>
+      <div className="mt-6">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+          Recomendados
+        </h2>
+
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map(barbershop => (
+           <BarberShopItem key={barbershop.id} barbershop={barbershop}/>
+          ))}
+        </div>
       </div>
     </div>
   );
