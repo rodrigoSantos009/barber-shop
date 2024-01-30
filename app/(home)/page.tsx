9bfa22559c1096dd78ab10607c5c7478373f15e4
rodrigoSantos009 <009rodrigo009@gmail.com>
2024-01-30 07:15:05 -0300

    feat: add inital search component

---
+++ app/(home)/page.tsx
@@ -0,0 +1,23 @@
+import { ptBR } from "date-fns/locale";
+import Header from "../_components/header";
+import { format } from "date-fns";
+import { Search } from "./__components/search";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Rodrigo!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+      <div className="px-5 mt-6">
+        <Search />
+      </div>
+    </div>
+  );
+}
